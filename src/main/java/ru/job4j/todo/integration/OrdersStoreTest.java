25ecee0eacb50ec83bb04ad16c380b36b2e211aa
ainz <ainz713@gmail.com>
2021-12-12 01:06:14 +0300

    Что такое интеграционное тестирование

---
+++ src/main/java/ru/job4j/todo/integration/OrdersStoreTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.todo.integration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class OrdersStoreTest {
+    private BasicDataSource pool = new BasicDataSource();
+
+    @Before
+    public void setUp() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(5);
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream("./db/update_001.sql")))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement("DROP TABLE IF EXISTS orders").executeUpdate();
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @Test
+    public void whenSaveOrderAndFindAllOneRowWithDescription() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertThat(all.size(), is(1));
+        assertThat(all.get(0).getDescription(), is("description1"));
+        assertThat(all.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void whenSaveOrderAndFindById() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+        store.save(Order.of("name2", "description2"));
+
+        Order all = store.findById(2);
+
+        assertThat(all.getDescription(), is("description2"));
+        assertThat(all.getId(), is(2));
+    }
+
+    @Test
+    public void whenSaveOrderAndFindByName() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+        store.save(Order.of("name2", "description2"));
+
+        Order all = store.findByName("name1");
+
+        assertThat(all.getDescription(), is("description1"));
+        assertThat(all.getId(), is(1));
+    }
+
+    @Test
+    public void whenSaveOrderAndUpdate() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+
+        boolean all = store.update(1, "name3", "description3");
+
+        assertThat(all, is(true));
+        assertThat(store.findById(1).getName(), is("name3"));
+    }
+}
